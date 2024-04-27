ec728f7a6ef90ad353c31a9dc598403652bed645
DanielRomero <dandres508@gmail.com>
2024-04-26 19:30:00 -0500

    flatten images

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -50,7 +50,7 @@ export class Product {
     @OneToMany(
         () => ProductImage,
         (productImage) => productImage.product,
-        {cascade: true} 
+        {cascade: true, eager: true} 
     )
     images?: ProductImage[];
 
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -45,20 +45,23 @@ export class ProductsService {
 
   }
 
-  findAll(paginationDto: PaginationDto) {
+  async findAll(paginationDto: PaginationDto) {
     
     const {limit = 10, offset= 0} = paginationDto;
     
-    return this.productRepository.find({
+    const product = await this.productRepository.find({
 
       take: limit,
       skip: offset,
-  
-      // TODO: relaciones
-
-
-
+      relations: {
+        images: true,
+      }
     })
+
+    return product.map(product => ({
+      ...product,
+      images: product.images.map(img => img.url)
+    }))
   }
 
   async findOne(term: string) {
@@ -68,12 +71,14 @@ export class ProductsService {
     if (isUUID(term)) {
       product = await this.productRepository.findOneBy({id: term});
     } else {
-      const queryBuilder = this.productRepository.createQueryBuilder();
+      const queryBuilder = this.productRepository.createQueryBuilder("prod");
       product = await queryBuilder
         .where(" UPPER (title) =:title or slug =:slug", {
           title:term.toUpperCase(),
           slug: term.toLowerCase(),
-        }).getOne();
+        })
+        .leftJoinAndSelect("prod.images", "prodImages")
+        .getOne();
     }
 
     if (!product) {
@@ -81,6 +86,14 @@ export class ProductsService {
     }
 
     return product;
+  }
+
+  async findOnePlain(term: string) {
+    const {images = [], ...rest} = await this.findOne(term);
+    return {
+      ...rest,
+      images: images.map(img => img.url)
+    }
   }
 
   async update(id: string, updateProductDto: UpdateProductDto) {
