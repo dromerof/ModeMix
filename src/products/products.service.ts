5684e8f2e7aad200199621be45ff769a0a4002ea
DanielRomero <dandres508@gmail.com>
2024-04-29 13:19:43 -0500

    queryRunner

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,6 +1,6 @@
 import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 
 
 import { CreateProductDto } from './dto/create-product.dto';
@@ -23,6 +23,8 @@ export class ProductsService {
 
     @InjectRepository(ProductImage)
     private readonly productImageRepository: Repository<ProductImage>,
+
+    private readonly dataSource: DataSource,
 
   ){}
   
@@ -98,27 +100,41 @@ export class ProductsService {
 
   async update(id: string, updateProductDto: UpdateProductDto) {
 
-    const product = await this.productRepository.preload({
-      id: id,
-      ...updateProductDto,
-      images: [],
-    });
+    const {images, ...toUpdate} = updateProductDto;
+
+    const product = await this.productRepository.preload({id, ...toUpdate});
 
     if (!product) {
       throw new NotFoundException(`Product with id: ${id} not found`);
     }
 
+    //Create query runner
+    const queryRunner =this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
     try {
-      
-      await this.productRepository.save(product);
-      return product;
+
+      if (images) {
+        await queryRunner.manager.delete(ProductImage, {product: {id}})
+        
+        product.images = images.map(img => this.productImageRepository.create({url: img}))
+      }
+
+      await queryRunner.manager.save(product);
+      await queryRunner.commitTransaction();
+      await queryRunner.release();
+
+      return this.findOnePlain(id);
 
     } catch (error) {
+
+      await queryRunner.rollbackTransaction();
+      await queryRunner.release();
 
       this.handleDBExceptions(error);
       
     }
-
 
   }
 
