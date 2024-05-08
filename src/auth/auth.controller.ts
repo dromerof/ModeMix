9f4a6815ac3108ece15712d4d90eae9eaa23a344
DanielRomero <dandres508@gmail.com>
2024-05-08 10:19:07 -0500

    added user login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateUserDto } from './dto/create-user.dto';
+
+import { LoginUserDto, CreateUserDto } from './dto';
 
 
 @Controller('auth')
@@ -8,8 +9,17 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post("register")
-  create(@Body() createUserDto: CreateUserDto) {
+  createUser(@Body() createUserDto: CreateUserDto) {
     return this.authService.create(createUserDto);
   }
+
+
+  @Post("login")
+  loginUser(@Body() loginUserDto: LoginUserDto) {
+    return this.authService.login(loginUserDto)
+  }
+
+
+
 
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,11 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import * as bcrypt from "bcrypt"
 
-import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
+import { LoginUserDto, CreateUserDto } from './dto';
 
 
 @Injectable()
@@ -40,6 +40,32 @@ export class AuthService {
     }
  
   }
+
+  async login(loginUserDto: LoginUserDto){
+
+    const {password, email} = loginUserDto;
+
+    const user = await this.userRepository.findOne({ 
+
+      where: {email},
+      select: {email: true, password: true}
+     })
+
+     if (!user) {
+      throw new UnauthorizedException("Credentials are not valid (email)");
+     }
+
+     if (!bcrypt.compareSync(password, user.password)) {
+      throw new UnauthorizedException("Credentials are not valid (password)");
+     }
+
+    return user;
+    //TODO: retornar el JWT
+
+  }
+
+
+
   
   private handleDBErrors(error: any): never{
 
